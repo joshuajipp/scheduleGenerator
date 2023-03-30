9011658b9a48614861aedd96cbcf468803f2a726
Joshua Jipp <joshuajipp@gmail.com>
2023-03-30 12:51:20 -0600

    high level implementation of Animal

--- edu/ucalgary/oop/Animal.java
+++ edu/ucalgary/oop/Animal.java
@@ -1,1 +1,25 @@
 package edu.ucalgary.oop;
+
+public class Animal {
+    private final int ID;
+    private final String NICKNAME;
+    private final String SPECIES;
+
+    public Animal(int id, String name, String species) {
+        this.ID = id;
+        this.NICKNAME = name;
+        this.SPECIES = species;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getNickname() {
+        return NICKNAME;
+    }
+
+    public String getSpecies() {
+        return SPECIES;
+    }
+}
