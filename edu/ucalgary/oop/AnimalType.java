22d0ac27363f64be182c602b951c976004dcfd41
Joshua Jipp <joshuajipp@gmail.com>
2023-03-30 12:37:37 -0600

    added enum to store unique data for each animal type

---
+++ edu/ucalgary/oop/AnimalType.java
@@ -0,0 +1,24 @@
+package edu.ucalgary.oop;
+
+public enum AnimalType {
+    COYOTE(19, 10, 5, 5),
+    FOX(0, 5, 5, 5),
+    PORCUPINE(19, 0, 5, 10),
+    BEAVER(8, 0, 5, 5),
+    RACCOON(0, 0, 5, 5);
+
+    private final int FEED_START_TIME;
+    private final int PREP_DURATION;
+    private final int FEED_DURATION;
+    private final int CLEAN_DURATION;
+
+    AnimalType(int feedStartTime, int prepDuration,
+            int feedDuration, int cleanDuration) {
+
+        this.FEED_START_TIME = feedStartTime;
+        this.PREP_DURATION = prepDuration;
+        this.FEED_DURATION = feedDuration;
+        this.CLEAN_DURATION = cleanDuration;
+
+    }
+}
