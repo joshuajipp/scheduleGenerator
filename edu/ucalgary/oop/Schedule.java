ef6bf2abdcfbb247c85693849d2500a29a39aaa4
mizybermas <mizybermas@DESKTOP-M0H94N5>
2023-03-30 15:36:56 -0600

    added private and public methods for schedule.java, taks.java and treatments.java

--- edu/ucalgary/oop/Schedule.java
+++ edu/ucalgary/oop/Schedule.java
@@ -1,1 +1,16 @@
 package edu.ucalgary.oop;
+
+import java.util.ArrayList;
+
+public class Schedule{
+    private String[][] timeArray;
+    private ArrayList<Treatments> treatmentArray;
+
+    public Schedule(String[][] timeArray, ArrayList<Treatments> treatmentArray){
+        this.timeArray = timeArray;
+        this.treatmentArray = treatmentArray;
+    }
+    public ArrayList<Treatments> getTreatmentArrayList() {
+            return treatmentArray;
+    }
+}
--- edu/ucalgary/oop/Treatments.java
+++ edu/ucalgary/oop/Treatments.java
@@ -1,1 +1,22 @@
 package edu.ucalgary.oop;
+
+public class Treatments{
+    private Animal animalID;
+    private Tasks taskID;
+    private int startHour;
+
+    public Treatments(Animal id, Tasks task, int startHour){
+        this.animalID = id;
+        this.taskID = task;
+        this.startHour = startHour;
+    }
+    public Animal getAnimalID(){
+        return animalID;
+    }
+    public Tasks getTaskID(){
+        return taskID;
+    }
+    public int getStartHour(){
+        return startHour;
+    }
+}
