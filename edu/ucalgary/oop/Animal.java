ef25a93f1053d3b7f63ac1493a0fb99bba32216e
Josh Jipp <jjipp@DESKTOP-8O8B4VH>
2023-03-31 16:55:37 -0600

    completed the Animal class constructor and changed params for Treatments constructor

--- edu/ucalgary/oop/Animal.java
+++ edu/ucalgary/oop/Animal.java
@@ -1,14 +1,53 @@
 package edu.ucalgary.oop;
 
+import java.sql.*;
+import java.util.ArrayList;
+
 public class Animal {
     private final int ID;
     private final String NICKNAME;
     private final String SPECIES;
+    private ArrayList<Treatments> animalTreatments = new ArrayList<Treatments>();
 
-    public Animal(int id, String name, String species) {
+    public Animal(int id, String name, String species, String user, String password) {
         this.ID = id;
         this.NICKNAME = name;
         this.SPECIES = species;
+        String url = "jdbc:mysql://localhost:3306/EWR";
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM TREATMENTS");
+
+            while (resultSet.next()) {
+                Treatments treatment = new Treatments(resultSet.getInt("TreatmentID"),
+                        resultSet.getInt("AnimalID"), resultSet.getInt("TaskID"),
+                        resultSet.getInt("StartHour"), user, password);
+
+                animalTreatments.add(treatment);
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public int getID() {
@@ -21,5 +60,9 @@ public class Animal {
 
     public String getSpecies() {
         return SPECIES;
+    }
+
+    public ArrayList<Treatments> getAnimalTreatments() {
+        return animalTreatments;
     }
 }
--- edu/ucalgary/oop/Treatments.java
+++ edu/ucalgary/oop/Treatments.java
@@ -1,22 +1,25 @@
 package edu.ucalgary.oop;
 
-public class Treatments{
+public class Treatments {
     private Animal animalID;
     private Tasks taskID;
     private int startHour;
 
-    public Treatments(Animal id, Tasks task, int startHour){
+    public Treatments(int treatmentID, int animalID, int taskID, int startHour) {
         this.animalID = id;
         this.taskID = task;
         this.startHour = startHour;
     }
-    public Animal getAnimalID(){
+
+    public Animal getAnimalID() {
         return animalID;
     }
-    public Tasks getTaskID(){
+
+    public Tasks getTaskID() {
         return taskID;
     }
-    public int getStartHour(){
+
+    public int getStartHour() {
         return startHour;
     }
 }
