05744a070e8100064cd27532f738b5cc00fb9e68
Josh Jipp <joshuajipp@gmail.com>
2023-04-01 13:18:29 -0600

    removed connection code from constructor

--- edu/ucalgary/oop/Animal.java
+++ edu/ucalgary/oop/Animal.java
@@ -9,39 +9,10 @@ public class Animal {
     private final AnimalType SPECIES;
     private ArrayList<Treatments> animalTreatments = new ArrayList<Treatments>();
 
-    public Animal(int id, String name, String species, String user, String password) {
+    public Animal(int id, String name, String species) {
         this.ID = id;
         this.NICKNAME = name;
         this.SPECIES = AnimalType.valueOf(species.toUpperCase());
-
-        // String url = "jdbc:mysql://localhost:3306/EWR";
-        // Connection connection = null;
-        // Statement statement = null;
-        // ResultSet resultSet = null;
-        // try {
-        // connection = DriverManager.getConnection(url, user, password);
-        // statement = connection.createStatement();
-        // String query = String.format("SELECT * FROM TREATMENTS WHERE AnimalID = %d",
-        // this.ID);
-        // resultSet = statement.executeQuery(query);
-
-        // while (resultSet.next()) {
-        // Treatments treatment = new Treatments(resultSet.getInt("TreatmentID"), id,
-        // resultSet.getInt("TaskID"), resultSet.getInt("StartHour"), user, password);
-
-        // animalTreatments.add(treatment);
-        // }
-        // } catch (SQLException e) {
-        // e.printStackTrace();
-        // } finally {
-        // try {
-        // resultSet.close();
-        // statement.close();
-        // connection.close();
-        // } catch (SQLException e) {
-        // e.printStackTrace();
-        // }
-        // }
     }
 
     public int getID() {
