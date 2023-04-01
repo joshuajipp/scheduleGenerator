a8465b460736f28373e1bbf66af1bcbcdcbd79a5
mizybermas <mizy.bermas@ucalgary.ca>
2023-03-31 18:07:49 -0600

    created constructor for schedule class to take in the username and password. Created method to get the ANIMALS from the database and created an Animal object with each row from the database, while adding it to the animal arrayList

--- edu/ucalgary/oop/Schedule.java
+++ edu/ucalgary/oop/Schedule.java
@@ -1,16 +1,56 @@
 package edu.ucalgary.oop;
 
+import java.sql.*;
 import java.util.ArrayList;
 
 public class Schedule{
     private String[][] timeArray;
-    private ArrayList<Treatments> treatmentArray;
+    private ArrayList<Animal> animalArray;
+    private Connection dbConnect;
+    private ResultSet results;
+    private String username;
+    private String password;
 
-    public Schedule(String[][] timeArray, ArrayList<Treatments> treatmentArray){
-        this.timeArray = timeArray;
-        this.treatmentArray = treatmentArray;
+    public Schedule(String username, String password){
+        this.username = username;
+        this.password = password;
     }
-    public ArrayList<Treatments> getTreatmentArrayList() {
-            return treatmentArray;
+    public void createConnection(){
+        try{
+            dbConnect = DriverManager.getConnection("jdbc:mysql://localhost/EWR", username, password);
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
     }
+    public ArrayList<Animal> selectAnimals() {
+        try{
+            Statement myStmt = dbConnect.createStatement();
+            results = myStmt.executeQuery("SELECT * FROM ANIMALS");
+            while (results.next()){
+                int id = results.getInt("AnimalID");
+                String nickname = results.getString("AnimalNickname");
+                String species = results.getString("AnimalSpecies");
+
+                Animal animal = new Animal(id, nickname, species, username, password);
+                animalArray.add(animal);
+            }   
+            myStmt.close();
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }
+        return animalArray;
+    }
+
+    public ArrayList<Animal>getTreatmentArrayList() {
+            return animalArray;
+    }
+    public void close(){
+        try{
+            results.close();
+            dbConnect.close();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    
 }
