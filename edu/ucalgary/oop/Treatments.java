862fb080ad59c42dd818ea4f8a8f3920d4358c71
Josh Jipp <joshuajipp@gmail.com>
2023-04-01 12:29:26 -0600

    removed auth from constructor for easier unit testing

--- edu/ucalgary/oop/Treatments.java
+++ edu/ucalgary/oop/Treatments.java
@@ -4,46 +4,55 @@ import java.sql.*;
 
 public class Treatments {
     private int treatmentID;
+    private int animalID;
     private int startHour;
     private String description;
     private int duration;
     private int maxWindow;
 
-    public Treatments(int treatmentID, int animalID, int taskID, int startHour, String user, String password) {
+    public Treatments(int treatmentID, int animalID, int startHour, String description, int duration, int maxWindow) {
         this.treatmentID = treatmentID;
+        this.animalID = animalID;
         this.startHour = startHour;
+        this.description = description;
+        this.duration = duration;
+        this.maxWindow = maxWindow;
 
-        String url = "jdbc:mysql://localhost:3306/EWR";
-        Connection connection = null;
-        Statement statement = null;
-        ResultSet resultSet = null;
-        try {
-            connection = DriverManager.getConnection(url, user, password);
-            statement = connection.createStatement();
-            String query = "SELECT * FROM ewr.treatments as treats"
-                    .concat("JOIN ewr.tasks tasks ON treats.TaskID = tasks.TaskID")
-                    .concat(String.format(
-                            "WHERE treats.TaskID = %d AND TreatmentID = %d", taskID, treatmentID));
-            resultSet = statement.executeQuery(query);
-            this.description = resultSet.getString("Description");
-            this.duration = resultSet.getInt("Duration");
-            this.maxWindow = resultSet.getInt("MaxWindow");
+        // String url = "jdbc:mysql://localhost:3306/EWR";
+        // Connection connection = null;
+        // Statement statement = null;
+        // ResultSet resultSet = null;
+        // try {
+        // connection = DriverManager.getConnection(url, user, password);
+        // statement = connection.createStatement();
+        // String query = "SELECT * FROM ewr.treatments as treats"
+        // .concat("JOIN ewr.tasks tasks ON treats.TaskID = tasks.TaskID")
+        // .concat(String.format(
+        // "WHERE treats.TaskID = %d AND TreatmentID = %d", taskID, treatmentID));
+        // resultSet = statement.executeQuery(query);
+        // this.description = resultSet.getString("Description");
+        // this.duration = resultSet.getInt("Duration");
+        // this.maxWindow = resultSet.getInt("MaxWindow");
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                resultSet.close();
-                statement.close();
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+        // } catch (SQLException e) {
+        // e.printStackTrace();
+        // } finally {
+        // try {
+        // resultSet.close();
+        // statement.close();
+        // connection.close();
+        // } catch (SQLException e) {
+        // e.printStackTrace();
+        // }
+        // }
     }
 
     public int getTreatmentID() {
         return treatmentID;
+    }
+
+    public int getAnimalID() {
+        return animalID;
     }
 
     public int getStartHour() {
