56c2a1ded384125b723f115b14367dac09c0dfc5
Josh Jipp <joshuajipp@gmail.com>
2023-04-01 13:42:40 -0600

    deleted unused constructor functionality

--- edu/ucalgary/oop/Treatments.java
+++ edu/ucalgary/oop/Treatments.java
@@ -1,7 +1,5 @@
 package edu.ucalgary.oop;
 
-import java.sql.*;
-
 public class Treatments {
     private int treatmentID;
     private int animalID;
@@ -17,34 +15,6 @@ public class Treatments {
         this.description = description;
         this.duration = duration;
         this.maxWindow = maxWindow;
-
-        // String url = "jdbc:mysql://localhost:3306/EWR";
-        // Connection connection = null;
-        // Statement statement = null;
-        // ResultSet resultSet = null;
-        // try {
-        // connection = DriverManager.getConnection(url, user, password);
-        // statement = connection.createStatement();
-        // String query = "SELECT * FROM ewr.treatments as treats"
-        // .concat("JOIN ewr.tasks tasks ON treats.TaskID = tasks.TaskID")
-        // .concat(String.format(
-        // "WHERE treats.TaskID = %d AND TreatmentID = %d", taskID, treatmentID));
-        // resultSet = statement.executeQuery(query);
-        // this.description = resultSet.getString("Description");
-        // this.duration = resultSet.getInt("Duration");
-        // this.maxWindow = resultSet.getInt("MaxWindow");
-
-        // } catch (SQLException e) {
-        // e.printStackTrace();
-        // } finally {
-        // try {
-        // resultSet.close();
-        // statement.close();
-        // connection.close();
-        // } catch (SQLException e) {
-        // e.printStackTrace();
-        // }
-        // }
     }
 
     public int getTreatmentID() {
