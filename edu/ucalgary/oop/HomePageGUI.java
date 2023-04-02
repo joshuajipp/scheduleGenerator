145df3d6251675ea9a50e5e87be5c238ddbdf994
mizybermas <mizy.bermas@ucalgary.ca>
2023-04-01 20:57:02 -0600

    added more documentation to the code

--- edu/ucalgary/oop/HomePageGUI.java
+++ edu/ucalgary/oop/HomePageGUI.java
@@ -12,49 +12,69 @@ import java.awt.*;
 import java.awt.event.*;
 import java.time.LocalDate;
 
-public class HomePageGUI extends JFrame implements ActionListener, MouseListener { 
-    private LocalDate date = LocalDate.now();
-    private JLabel scheduleHeader;
+/*
+HomePageGUI is a class that extends JFrame and implements ActionListener, and MouseListener. It displays the 
+homepage for the wildlife rescue program that will show the schedules for the volunteers.
+*/
 
+public class HomePageGUI extends JFrame implements ActionListener, MouseListener { 
+    private LocalDate date = LocalDate.now(); //current date
+    private JLabel scheduleHeader; //label for schedule header
+
+    /*
+    HomePageGUI constructor sets up the Graphical User Interface by creating the window for the GUI by initializing its size
+    and the title. It also calls setupGUI() to add buttons or text in the page.
+     */
     public HomePageGUI(){
-        super("Wildlife Rescue");
-        setupGUI();
-        setSize(800,500);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        super("Wildlife Rescue"); //call to JFrame constructor with title arguement
+        setupGUI(); //calls to setupGUI method
+        setSize(800,500); //sets the size of the JFrame
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //set default close operation
     }
+    /*
+    This constructor sets up the GUI by creating and configurating the Schedule, Animals, Treatments and Tasks buttons, as well as 
+    the schedule header lable which shows the date of which the schedule is being made for.
+     */
     public void setupGUI(){
-        JButton ScheduleButton  = new JButton("Schedule");
+        //create and configure the Schedule Button
+        JButton ScheduleButton  = new JButton("Schedule"); 
         ScheduleButton.setFont(new Font("Calibri",Font.PLAIN,30));
         ScheduleButton.addActionListener(this);
         
+        //create and configure the Schedule Header to show the date for the schedule
         scheduleHeader = new JLabel("Schedule for " + date.plusDays(1));
         scheduleHeader.setFont(new Font("Calibri", Font.BOLD,30));
         scheduleHeader.setForeground(Color.BLUE);
 
-
+        //create and configure the Animal Button
         JButton AnimalButton = new JButton("Animals");
         AnimalButton.setFont(new Font("Calibri",Font.PLAIN,30));
         AnimalButton.addActionListener(this);
 
+        //create and configure the Treatments Button
         JButton TreatmentsButton =  new JButton("Treatments");
         TreatmentsButton.setFont(new Font("Calibri",Font.PLAIN,30));
         TreatmentsButton.addActionListener(this);
 
+        //create and configure the Tasks Button
         JButton TasksButton = new JButton("Tasks");
         TasksButton.setFont(new Font("Calibri",Font.PLAIN,30));
         TasksButton.addActionListener(this);
 
+        //create and configure headerpanel,mainbody
         JPanel headerPanel = new JPanel();
         headerPanel.setLayout(new FlowLayout());
         JPanel mainBody = new JPanel();
         mainBody.setLayout(new FlowLayout());
 
+        //added the buttons to the headerPanel and label to mainBody
         headerPanel.add(ScheduleButton);
         headerPanel.add(AnimalButton);
         headerPanel.add(TreatmentsButton);
         headerPanel.add(TasksButton);
         mainBody.add(scheduleHeader);
 
+        //added headerPanel and mainBody to the JFrame with relative positions
         this.add(headerPanel, BorderLayout.NORTH);
         this.add(mainBody, BorderLayout.LINE_START);
 
@@ -69,6 +89,10 @@ public class HomePageGUI extends JFrame implements ActionListener, MouseListener
     public void mouseExited(MouseEvent event){}
     public void mousePressed(MouseEvent event){}
     public void mouseReleased(MouseEvent event){}
+    /*
+    Invokes the GUI. Calls the HomePageGUI() constructor to create and configure the JFrame and this main method sets the visibility
+    to true.
+     */
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
             new HomePageGUI().setVisible(true);        
