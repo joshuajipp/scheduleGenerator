4d9b9f590affb0d78f9265269d3a233003f53354
Joshua Koshy <joshua.koshy@ucalgary.ca>
2023-04-02 16:37:45 -0600

    Added test file with sample test, added jar files

---
+++ edu/ucalgary/oop/ExistingFunctionalityTest.java
@@ -0,0 +1,19 @@
+/**
+@author 	Joshua Koshy <a href="mailto:joshua.koshy@ucalgary.ca">joshua.koshy@ucalgary.ca</a>
+@author 	Nicole Lazarte <a href="mailto:nicole.lazarte@ucalgary.ca">nicole.lazarte@ucalgary.ca</a>
+@version    	1.0
+@since  	1.0
+*/
+package edu.ucalgary.oop;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ExistingFunctionalityTest {
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
