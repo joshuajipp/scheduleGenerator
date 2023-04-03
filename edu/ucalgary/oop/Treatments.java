e8b8d19c7494dac85c1e97e1cfee36b35d0bdae3
mizybermas <mizy.bermas@ucalgary.ca>
2023-04-02 19:01:50 -0600

    added some documentations to the treatment class

--- edu/ucalgary/oop/Treatments.java
+++ edu/ucalgary/oop/Treatments.java
@@ -1,3 +1,9 @@
+/**
+@author 	Mizy Bermas <a href="mailto:mizy.bermas@ucalgary.ca">mizy.bermas@ucalgary.ca</a>
+@author 	Joshua Jipp <a href="mailto:joshua.jipp@ucalgary.ca">joshua.jipp@ucalgary.ca</a>
+@version    	1.1
+@since  	    1.0
+*/
 package edu.ucalgary.oop;
 
 public class Treatments {
@@ -7,7 +13,7 @@ public class Treatments {
     private int duration;
     private int maxWindow;
     private int setupTime;
-
+    /*Constructor */
     public Treatments(int animalID, int startHour,
             String description, int duration, int maxWindow) {
 
@@ -18,7 +24,7 @@ public class Treatments {
         this.maxWindow = maxWindow;
         this.setupTime = 0;
     }
-
+    /*Constructor */
     public Treatments(int animalID, int startHour,
             String description, int duration, int maxWindow, int setupTime) {
 
@@ -29,27 +35,27 @@ public class Treatments {
         this.maxWindow = maxWindow;
         this.setupTime = setupTime;
     }
-
+    /*Getter*/
     public int getAnimalID() {
         return animalID;
     }
-
+    /*Getter*/
     public int getStartHour() {
         return startHour;
     }
-
+    /*Getter */
     public String getDescription() {
         return description;
     }
-
+    /*Getter*/
     public int getDuration() {
         return duration;
     }
-
+    /*Getter*/
     public int getMaxWindow() {
         return maxWindow;
     }
-
+    /*Getter*/
     public int getSetupTime() {
         return setupTime;
     }
