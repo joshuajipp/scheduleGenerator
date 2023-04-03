361145beb513df3cff267e6f80881d1d864b794d
Joshua Koshy <joshua.koshy@ucalgary.ca>
2023-04-03 14:01:40 -0600

    Animal "for" test case, still error checking

--- edu/ucalgary/oop/ExistingFunctionalityTest.java
+++ edu/ucalgary/oop/ExistingFunctionalityTest.java
@@ -9,11 +9,30 @@ package edu.ucalgary.oop;
 import org.junit.*;
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class ExistingFunctionalityTest {
 
-    @Test
+    /**@Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }*/
+    @Test
+    public void testAnimaltwonames() {        
+        ArrayList<Treatments> animalTreatments = new ArrayList<Treatments>();
+        ArrayList<Treatments> expanimalTreatments = new ArrayList<Treatments>();
+        Animal kits = new Animal(1, "Sherlock Holmes", "Fox");
+        AnimalType expectedanimal = AnimalType.KITS;
+
+        animalTreatments = kits.getAnimalTreatments();
+        expanimalTreatments.add(new Treatments(1, 0, String.format(
+            "Clean cage for the Kits", kits.getNickname()), 5, 24));
+
+        assertEquals(expectedanimal, kits.getSpecies());
+        /**assertEquals(expanimalTreatments, animalTreatments);*/
+        assertEquals(new HashSet<Treatments>(expanimalTreatments), new HashSet<Treatments>(animalTreatments));
+
     }
 }
