28095e1bf237cf05c3283bed612894f3900b4ade
Joshua Koshy <joshua.koshy@ucalgary.ca>
2023-04-05 03:28:39 -0600

    Testing of animal constructor done

--- edu/ucalgary/oop/ExistingFunctionalityTest.java
+++ edu/ucalgary/oop/ExistingFunctionalityTest.java
@@ -16,6 +16,8 @@ import java.util.HashSet;
 
 public class ExistingFunctionalityTest {
 
+    /* @Before */
+
     /**@Test
     public void shouldAnswerWithTrue()
     {
@@ -27,10 +29,38 @@ public class ExistingFunctionalityTest {
      */
     public void testanimalcorrectinputonename() {
         Animal rightanimal = new Animal(1, "Fluffy", "Porcupine");
+
         assertEquals(1, rightanimal.getID());
         assertEquals("Fluffy", rightanimal.getNickname());
         assertEquals(AnimalType.PORCUPINE, rightanimal.getSpecies());
 
+    }
+
+    @Test
+    /*
+     * Singular animal name given, species remains unchanged as specified and treatments arraylist updated with feeding and cage cleaning 
+     */
+    public void testAnimalelseconditiontreatments() {
+        Animal rightanimal = new Animal(1, "Fluffy", "Porcupine");
+        ArrayList <Treatments> testtreatments = rightanimal.getAnimalTreatments();
+        ArrayList <Treatments> exptreatments = new ArrayList<Treatments>();
+        exptreatments.add(new Treatments(1, 19, "Feed Fluffy", 5,
+        3, 0));
+        exptreatments.add(new Treatments(1, 0, "Clean cage for Fluffy", 10, 24));
+
+        for (int i = 0; i < testtreatments.size(); i++) {
+            Treatments test = testtreatments.get(i);
+            Treatments exp = exptreatments.get(i);
+        
+            assertEquals(exp.getAnimalID(), test.getAnimalID()); /* Test case runs */
+            assertEquals(exp.getStartHour(), test.getStartHour()); /* Test case runs */
+            assertEquals(exp.getDescription(), test.getDescription()); /* Test case runs */
+            assertEquals(exp.getDuration(), test.getDuration()); /* Test case runs */
+            assertEquals(exp.getMaxWindow(), test.getMaxWindow()); /* Test case runs */
+            if (i==1) {
+                assertEquals(exp.getSetupTime(), test.getSetupTime());
+            }
+        }
     }
 
     @Test
@@ -43,37 +73,28 @@ public class ExistingFunctionalityTest {
         AnimalType expectedanimal = AnimalType.KITS;
         ArrayList <Treatments> testtreatments = animal.getAnimalTreatments();
         ArrayList <Treatments> exptreatments = new ArrayList<Treatments>();
-        exptreatments.add(new Treatments(5, 0, "Feed Sherlock Holmes", 0,
-        3, 0));
-        exptreatments.add(new Treatments(5, 0, "Clean cage for Sherlock Holmes", 30, 24));
+        exptreatments.add(new Treatments(5, 0, "Clean cage for the Kits", 30, 24));
+        Treatments test = testtreatments.get(0);
+        Treatments exp = exptreatments.get(0);
 
         assertEquals(expectedanimal, animal.getSpecies());
-
-        for (int i = 0; i < testtreatments.size(); i++) {
-            Treatments test = testtreatments.get(i);
-            Treatments exp = exptreatments.get(i);
-        
-            assertEquals(exp.getAnimalID(), test.getAnimalID());
-            assertEquals(exp.getStartHour(), test.getStartHour());
-            assertEquals(exp.getDescription(), test.getDescription());
-            assertEquals(exp.getDuration(), test.getDuration());
-            assertEquals(exp.getMaxWindow(), test.getMaxWindow());
-            if (i==1) {
-                assertEquals(exp.getSetupTime(), test.getSetupTime());
-            }
-        }
+        assertEquals(exp.getAnimalID(), test.getAnimalID());
+        assertEquals(exp.getStartHour(), test.getStartHour());
+        assertEquals(exp.getDescription(), test.getDescription()); /*shouldn't description include pet name? */
+        assertEquals(exp.getDuration(), test.getDuration());
+        assertEquals(exp.getMaxWindow(), test.getMaxWindow());
+       
        
 
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)    
     /*
-     * Singular animal name given, species remains unchanged as specified and treatments arraylist updated with feeding and cage cleaning 
+     * Testing species not included in enum gives IllegalArgumentException
      */
-    public void testAnimalelsecondition() {
-        assertTrue( true );
+    public void testAnimalConstructorWithInvalidSpecies() {
+    Animal wrongspecies = new Animal(1, "Hiss", "Basilisk");
     }
 
-    /*@Test
-    public void*/ 
+
 }
