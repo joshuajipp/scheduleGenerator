ea48bdf0ec79effe8741d466dd37309135877f73
mizybermas <mizy.bermas@ucalgary.ca>
2023-04-05 17:34:30 -0600

    successully printing schedule to the GUI when volunteer is not needed

--- edu/ucalgary/oop/HomePageGUI.java
+++ edu/ucalgary/oop/HomePageGUI.java
@@ -8,19 +8,20 @@ package edu.ucalgary.oop;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-
 import java.sql.*;
-import java.awt.*;
-import java.awt.event.*;
 import java.time.LocalDate;
+import java.awt.*;
+import java.io.*;
+import java.util.*;
 
 /*
 HomePageGUI is a class that extends JFrame and implements ActionListener, and MouseListener. It displays the 
 homepage for the wildlife rescue program that will show the schedules for the volunteers.
 */
 
-public class HomePageGUI extends JFrame implements ActionListener, MouseListener { 
-    private LocalDate date = LocalDate.now(); 
+public class HomePageGUI extends JFrame  { 
+    private LocalDate date = LocalDate.now();
+    ArrayList<String> scheduleList = new ArrayList<String>();
 
 
     /*
@@ -29,6 +30,14 @@ public class HomePageGUI extends JFrame implements ActionListener, MouseListener
      */
     public HomePageGUI(){
         super("Wildlife Rescue"); //call to JFrame constructor with title arguement
+        setupGUI(); //calls to setupGUI method
+        setSize(800,500); //sets the size of the JFrame
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //set default close operation
+    }
+    public HomePageGUI(ArrayList<String> scheduleList){
+    
+        super("Wildlife Rescue"); //call to JFrame constructor with title arguement
+        this.scheduleList = scheduleList;
         setupGUI(); //calls to setupGUI method
         setSize(800,500); //sets the size of the JFrame
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //set default close operation
@@ -42,11 +51,17 @@ public class HomePageGUI extends JFrame implements ActionListener, MouseListener
         tabbedPane.setFocusable(false);
         this.add(tabbedPane);
 
+        JPanel schedulePanel = new JPanel(new BorderLayout());
         JLabel scheduleLabel = new JLabel("Schedule for: " + date.plusDays(1));
-        //String fromsched = Schedule.label();
-        //JLabel scheduleLabel = new JLabel(fromsched);
+        schedulePanel.add(scheduleLabel,BorderLayout.NORTH);
+        JList<String> scheduleJList = new JList<>(scheduleList.toArray(new String[0]));
+        JScrollPane scrollPanel = new JScrollPane(scheduleJList);
+        scrollPanel.setPreferredSize(new Dimension(800,300));
+       
+        schedulePanel.add(scrollPanel,BorderLayout.CENTER);
+      
         JLabel animalLabel = new JLabel ("This is the animal tab");
-    
+     
 
         JPanel treatmentPanel = new JPanel(new BorderLayout());
         JTable table  = treatmentTable();
@@ -54,13 +69,13 @@ public class HomePageGUI extends JFrame implements ActionListener, MouseListener
         scrollPane.setPreferredSize(new Dimension(800,300));
         treatmentPanel.add(scrollPane);
        
-        tabbedPane.add("Schedule", scheduleLabel);
+        tabbedPane.add("Schedule", schedulePanel);
         tabbedPane.add("Animal", animalLabel);
         tabbedPane.add("Treatment", treatmentPanel);
-
-
        
     }
+   
+  
     public JTable treatmentTable(){
 
         DefaultTableModel tableModel = new DefaultTableModel();
@@ -97,60 +112,68 @@ public class HomePageGUI extends JFrame implements ActionListener, MouseListener
         return table;
        
     }
-
-    /*
-    
-     */
-    public void actionPerformed(ActionEvent event){
-      //handle the ok
+     public ArrayList<String> schedReadFile(){
+        try{
+            File schedFile = new File("sample.txt");
+            Scanner schedReader = new Scanner(schedFile);
+            while(schedReader.hasNextLine()){
+                String data = schedReader.nextLine();
+                scheduleList.add(data);
+            }
+            schedReader.close();
+            return scheduleList;
+        }catch(FileNotFoundException e){
+            System.out.println("An error occured.");
+            e.printStackTrace();
+        }return null;
+        
     }
-    
-    public void mouseClicked(MouseEvent event){
-      
-    }
-
-    public void mouseEntered(MouseEvent event){}
-    public void mouseExited(MouseEvent event){}
-    public void mousePressed(MouseEvent event){}
-    public void mouseReleased(MouseEvent event){}
     /*
     Invokes the GUI. Calls the HomePageGUI() constructor to create and configure the JFrame and this main method sets the visibility
     to true.
     @param args Optional command-line argument
      */
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) { 
         EventQueue.invokeLater(() -> {
             new HomePageGUI().setVisible(true);        
         });
+
         Schedule.main(args);
         //String[] volunteerChecker = Schedule.main(args); //return either {"true"} or {"false","1,2"}
         String[] volunteerChecker = {"false","1,2"};
         String boolCheck = volunteerChecker[0];
-        if (boolCheck == "true"){
-            //read from the txt file
-            //display on GUI
-            //create instance of class to access method
-
+        if (boolCheck.equals("true")){
+            HomePageGUI homePage = new HomePageGUI();
+            ArrayList<String> scheduleList = homePage.schedReadFile();
+            EventQueue.invokeLater(() -> {
+                new HomePageGUI(scheduleList).setVisible(true);        
+            });
+           
         }
-        if (boolCheck == "false"){
+    
+    
+        if (boolCheck.equals("false")){
+           
             String volunteerTime = volunteerChecker[1];
             String[] arrOfVolunTime = volunteerTime.split(",");
             String message = "Vounteer is needed for the following times: \n";
             for (String time : arrOfVolunTime){
                 message += time + "\n";
             }
-            JOptionPane.showMessageDialog(null, message);   
-           
+            JOptionPane optionPane = new JOptionPane();
+            optionPane.setMessage("Volunteer is needed for the following hour: "+ message);
+            optionPane.setMessageType(JOptionPane.WARNING_MESSAGE);
+            Object[] option = {"Confirm"};
+            optionPane.setOptions(option);
+            JDialog dialog = optionPane.createDialog(null, "Custom Option Pane");
+            dialog.setVisible(true);
+            Object selectedValue = optionPane.getValue();
+            if(selectedValue.equals("Confirm")){
+            // Do something if the "Yes, please" button is clicked
+            }
+            
         }
-
-        //getting return here from schedule(string = boolean)
-        //get false if volunteer list have items (need volunteer) {"false","1,3"} 
-        //need pop up window that will ask for volunteer confirmation for the hours. On click rerun main with "false"
-        //get true if schedule is created 
-        // if true read from txt file.
-
         
     }
-    
+   
 }
